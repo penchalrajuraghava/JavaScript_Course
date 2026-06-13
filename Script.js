7298e05837be4b9c758835b138c4b29764998d25
penchalrajuraghava <penchalrajuraghava@gmail.com>
2026-06-13 10:55:31 +0530

    Values and variables

---
+++ Script.js
@@ -0,0 +1,56 @@
+// values and variables
+// Naming conventions
+
+let js = "Amazing";
+console.log(42 + 10 + 23 - 10);
+
+console.log("penchal raju");
+
+let firstname = "penchal raju";
+
+console.log(firstname);
+console.log(firstname);
+
+firstname = "Deepika";
+console.log(firstname);
+
+let firstNamePerson; // work
+
+let first_name_person; // recommended
+
+//let 3years = 3  not recommended invalid or unexpected token
+
+// let nam3&hen = "js"; // unexpected token &
+
+//let function = 22 unexpected token function
+
+// better naming conventions is
+
+/* 
+1. using _ instead 
+2. use $ symbol
+3. first_name (work easy understanding)
+4. 4lastname (error not recommended)
+
+*/
+
+let pi = 3456;
+
+console.log(pi);
+
+/* Better using naming conventions */
+
+// easy understanding
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+/* bad conventions was */
+
+// typically hard to understand
+
+let job1 = "programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
